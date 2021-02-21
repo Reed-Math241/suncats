bd5a40fa920e6828301d64d458e70c960206d2c9
ahnsb5117 <ahnsb5117@gmail.com>
2021-02-20 23:55:23 -0800

    organizing code

--- data-raw/cat_reg.R
+++ data-raw/cat_reg.R
@@ -9,10 +9,7 @@ Registered_Cats <- readr::read_csv("https://data.sunshinecoast.qld.gov.au/api/vi
   select(-SpecificBreed) %>% 
   mutate(PrimaryColour = gsub('([[:upper:]])', ' \\1', PrimaryColour)) %>%
   mutate(FirstColour = gsub("([A-Za-z]+).*", "\\1", PrimaryColour)) %>% 
-
 Registered_Cats
-
-mutate(CRASH_MO_NO_SEA = case_when(CRASH_MO_NO == "01" ~ "Winter", 
 
 usethis::use_data(DATASET, overwrite = TRUE)
 
