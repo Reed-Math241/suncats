712beea67124adba7ac29208117e954e8db9c750
ahnsb5117 <ahnsb5117@gmail.com>
2021-03-02 14:36:08 -0800

    not done

---
+++ suncats_help.R
@@ -0,0 +1,25 @@
+#' Sunshine Coast Cats (2016 - Present)
+#' 
+#' Data on Cats in Sunshine Coast Region
+#' 
+#' @docType data
+#' @name suncats
+#' @usage data(suncats)
+#' @format  A data frame with 9919 observations on the following 8 variables.
+#' \itemize{
+#' \item{\code{AnimalType}}{ price (US dollars)}
+#' \item{\code{Name}}{ size of lot (acres)}
+#' \item{\code{PrimaryBreed}}{ age of house (years)}
+#' \item{\code{PrimaryColour}}{ value of land (US dollars)}
+#' \item{\code{De-sexed}}{ living are (square feet)}
+#' \item{\code{Gender}}{ percent of neighborhood that graduated college}
+#' \item{\code{Age}}{ number of bedrooms}
+#' \item{\code{Locality}}{ number of fireplaces}
+#' \item{\code{FirstColour}}{ number of bathrooms (half bathrooms have no shower or tub)}
+#' }
+#' @source 
+#' Data collected by Sunshine Coast Council and used in the "Stat 101" case study 
+#' "How much is a Fireplace Worth".  
+#' See also 
+#' <https://www.saratogacountyny.gov/departments/real-property-tax-service-agency/>
+NA
